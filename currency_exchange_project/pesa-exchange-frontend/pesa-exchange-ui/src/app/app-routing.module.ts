0a11473d802266b84aaac9dbea1316223bb9b556
jimmy <jimmy.aduvagah@gmail.com>
2021-06-22 20:29:21 +0300

    create registration component

--- currency_exchange_project/pesa-exchange-frontend/pesa-exchange-ui/src/app/app-routing.module.ts
+++ currency_exchange_project/pesa-exchange-frontend/pesa-exchange-ui/src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AccountsComponent } from './accounts/accounts.component';
+import { AuthComponent } from './auth/auth.component';
+import { ProfileComponent } from './profile/profile.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+    {path: 'login', component: AuthComponent},
+    {path: 'accounts', component: AccountsComponent},
+    {path: 'profile', component: ProfileComponent},
+    {path: '', redirectTo: '/accounts', pathMatch: 'full'},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
